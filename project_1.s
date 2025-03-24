a1f997bea994e8c0d486db25e08b32afb3a24f7d
Dimitri Cromarty <dimitricromarty@Dimitris-MacBook-Air.local>
2025-03-23 21:13:22 -0400

    We started off the main function by reading the string

--- project_1.s
+++ project_1.s
@@ -4,3 +4,11 @@ n_a: .asciiz "N/A"
 
 .text
 .globl main
+
+main:
+    #Read input string
+    li $v0, 8   #syscall 8 :read string
+    la $a0, input_buffer #load buffer address
+    li $a1, 11 #read up to 10 characters
+    syscall
+
